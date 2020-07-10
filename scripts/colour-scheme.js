f982371f3cb317b6a53855500527d37ab40b7ad8
Alan Rempel <alan.rempel@gmail.com>
2020-07-10 12:22:00 -0400

    Add Google analytics, font size slider

--- scripts/colour-scheme.js
+++ scripts/colour-scheme.js
@@ -1,42 +1,40 @@
+'use strict';
+
 var prefix = "colour-scheme-";
-var storageKey = "colour-scheme";
-var colourSchemeSelectId = "colour-scheme-select";
-var settingsPanelToggleId = "settings-toggle";
-var settingsPanelId = "settings-panel";
-var settingsPanelCloseButtonId = "settings-close";
+var colourSchemeClassNames = ['auto', 'light', 'dark'].map(function(c) {return prefix.concat(c)});
 
-function setColourScheme(element, colourScheme) {
-  var classes = element.getAttribute("class").split(" ");
-  var newClasses = classes.filter(className => !className.startsWith(prefix));
-  var colourSchemeClassName = prefix + colourScheme;
-  newClasses.push(colourSchemeClassName);
-  element.setAttribute("class", newClasses.join(" "));
-  window.localStorage.setItem(storageKey, colourScheme);
+function setColourScheme(colourScheme) {
+  var element = document.querySelector("body");
+  var colourSchemeClassName = prefix.concat(colourScheme);
+  element.classList.remove(...colourSchemeClassNames);
+  element.classList.add(colourSchemeClassName);
 }
 
-function addSelectOnChangeEvent(element, select) {
-  select.onchange = function(event) {
-    setColourScheme(element, event.target.value);
+function setFontSize(size) {
+  document.documentElement.style.setProperty("--base-font-size", size + "rem");
+}
+
+function setUpControl(elementId, eventName, applyValue) {
+  var control = document.getElementById(elementId);
+  if (!control) {
+    console.log("Error! Element with id " + elementId + "not found!");
   }
-}
-
-function getSavedColourScheme(element, select) {
-  var stored = window.localStorage.getItem(storageKey);
+  var stored = window.localStorage.getItem(elementId);
   if (stored) {
-    select.value = stored;
-    setColourScheme(element, stored);
+    applyValue(stored);
+    control.value = stored;
   }
-};
-
-function setUpColourSchemeSystem() {
-  var select = document.getElementById(colourSchemeSelectId);
-  var body = document.querySelector("body");
-  addSelectOnChangeEvent(body, select);
-  getSavedColourScheme(body, select);
+  var callback = function(event) {
+    var val = event.target.value;
+    applyValue(val);
+    window.localStorage.setItem(elementId, val);
+  };
+  control.addEventListener(eventName, callback, {passive: true});
 }
 
 // This script should have the 'defer' attribute set, so that the
 // 'DOMContentLoaded' event will not yet have fired when it is run.
 document.addEventListener("DOMContentLoaded", function(_event) {
-  setUpColourSchemeSystem();
+  setUpControl("font-size", "input", setFontSize);
+  setUpControl("colour-scheme", "change", setColourScheme);
 });
